f6b5512b218af3c25bb90b6483b043884f483e6b
Mathakgale Elmus Mohale <mathakgalemohale@gmail.com>
2021-09-17 05:21:03 +0200

    Task 0.6 first attempt

---
+++ task6.py
@@ -0,0 +1,19 @@
+
+
+def maximum (num1, num2, num3):
+	largest  = 0
+	if  largest  < num1:
+		largest = num1
+	if largest < num2:
+		largest = num2
+	if largest < num3:
+		largest = num3
+	return largest
+
+
+max_number = maximum(33,2,78)
+print(max_number)
+max_number = maximum(3,22,8)
+print(max_number)
+max_number = maximum(33,2,7)
+print(max_number)
