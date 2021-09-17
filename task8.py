b3ab0b258ce17c986b234edc9bee527cf15e6a88
Mathakgale Elmus Mohale <mathakgalemohale@gmail.com>
2021-09-17 13:24:08 +0200

    fixe result presantation and errors

--- task8.py
+++ task8.py
@@ -5,15 +5,26 @@
 def compute (number):
 	hours = 0
 	minutes = 0
-	while number > 60:
+	while number >= 60:
 		hours = hours+1
 		number=number-60
 	minutes = number
 
-	print(f" {hours} hours and {minutes} minutes")
+	if minutes == 0 :
+		if hours == 1:
+			print(f" {hours} hour")
+		else:
+			print(f" {hours} hours")
+	else:
+		if hours == 1:
+			print(f" {hours} hour, {minutes} minutes")
+		else:
+			print(f" {hours} hours, {minutes} minutes")
 
 
 
 compute(100)
 compute(133)
 compute(71)
+compute(60)
+
