f49dbb44197e1c2c576c1694da9d6e7c2c43c297
Mathakgale Elmus Mohale <mathakgalemohale@gmail.com>
2021-09-17 14:08:02 +0200

    fixed mistakes

--- task9.py
+++ task9.py
@@ -3,15 +3,24 @@ def find_vowels(word):
 	vowels = ['a','e','i','o','u']
 	word = word.lower()
 	found_vowels = []
+
 	# finds vowels in string
-	print ("Vowels : ",end="")
 	for i in vowels:
 		for j in word:
 			if i == j:
-				print(i+" ",end="")
+				found_vowels.append(i)
 				break
-	print()
 
+	# This section print the results
+	print ("Vowels: ",end="")
+	count = 0
+	for i in found_vowels:
+		if count >= len(found_vowels)-1:
+			print(f'{i}')
+		else:
+			print(f"{i}, ",end="")
+		count=count+1 
+		
 
 find_vowels("Umuzi")
 find_vowels("theeeesone")
