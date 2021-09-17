ce2eb9c16a2dd05e9aaa6c5a3ecc8282a12b9e65
Mathakgale Elmus Mohale <mathakgalemohale@gmail.com>
2021-09-17 14:01:42 +0200

    fixed mistake, code gives correct output

--- task10.py
+++ task10.py
@@ -5,13 +5,22 @@
 def common_letters(word_1, word_2):
 	word_1 = word_1.lower()
 	word_2 = word_2.lower()
-	print ("Common letters: ",end="")
+	found_chars = []
+	#This section finds the matching letters
 	for i in word_1:
 		for j in word_2:
 			if i == j:
-				print(i+" ",end="")
+				found_chars.append(i)
 				break
-	print()
+	# This section print the results
+	print ("Common letters: ",end="")
+	count = 0
+	for i in found_chars:
+		if count >= len(found_chars)-1:
+			print(f'{i}')
+		else:
+			print(f"{i}, ",end="")
+		count=count+1 
 
 
 common_letters ("house", "computers")
