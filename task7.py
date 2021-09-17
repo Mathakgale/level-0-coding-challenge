166725c91be63f3399abdaec301f5620ad522063
Mathakgale Elmus Mohale <mathakgalemohale@gmail.com>
2021-09-17 05:33:13 +0200

    task 7 first attempt

---
+++ task7.py
@@ -0,0 +1,17 @@
+"""
+	covert temperature to fahrenhiet
+"""
+def celsius_to_fahrenhiet(temp):
+	return (9/5)*temp +32
+
+"""
+ convert temperature to celsius
+"""
+def fahrenhiet_to_celsius (temp):
+	return (temp-32)*(5/9)
+
+celsius = fahrenhiet_to_celsius (34)
+print (f"in celsius {celsius}")
+
+fahrenhiet = celsius_to_fahrenhiet(celsius)
+print (f"in fahrenhiet {fahrenhiet}")
