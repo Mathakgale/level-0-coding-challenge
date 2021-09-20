58850667ad1b67e610fd55e5c04c8cae7e8bc719
Mathakgale Elmus Mohale <mathakgalemohale@gmail.com>
2021-09-20 14:55:59 +0200

    fixed errors on Task0.8

--- task8.py
+++ task8.py
@@ -1,30 +1,37 @@
 
-"""
-	This function convert any number into hours and minutes
-"""
-def compute (number):
+def get_min_string(minutes):
+	if minutes == 1:
+		return f'{minutes} minute'
+	elif minutes == 0:
+		return''
+	return f'{minutes} minutes'
+
+def get_hour_string(hour):
+	if hour == 1:
+		return f'{hour} hour'
+	elif hour == 0:
+		return ''
+	return f'{hour} hours'
+
+def convert_to_hrs_min (number):
 	hours = 0
 	minutes = 0
 	while number >= 60:
 		hours = hours+1
 		number=number-60
 	minutes = number
-
-	if minutes == 0 :
-		if hours == 1:
-			print(f" {hours} hour")
-		else:
-			print(f" {hours} hours")
+	if hours == 0:
+		print(get_min_string(minutes))
 	else:
-		if hours == 1:
-			print(f" {hours} hour, {minutes} minutes")
-		else:
-			print(f" {hours} hours, {minutes} minutes")
+		print(f'{get_hour_string(hours)} {get_min_string(minutes)}')
 
 
 
-compute(100)
-compute(133)
-compute(71)
-compute(60)
-
+convert_to_hrs_min(100)
+convert_to_hrs_min(133)
+convert_to_hrs_min(71)
+convert_to_hrs_min(60)
+convert_to_hrs_min(30)
+convert_to_hrs_min(1)
+convert_to_hrs_min(61)
+convert_to_hrs_min(121)
