08c9708ca6198982f370a7000d9b453837fe6f82
Mathakgale Elmus Mohale <mathakgalemohale@gmail.com>
2021-09-20 14:24:25 +0200

    fixed mistakes in task 0.5

--- task5.py
+++ task5.py
@@ -1,10 +1,9 @@
-import math 
 
 # considering tringle of side a,b and d
 
 def area (a,b,c):
-	s = 0.5*(a+b+c)							#calculates the semi-parimeter
-	area = (s*(s-a)*(s-b)*(s-c))**(1/2)		# calculates the area
+	semi_parimeter = 0.5*(a+b+c)						
+	area = (semi_parimeter*(semi_parimeter-a)*(semi_parimeter-b)*(semi_parimeter-c))**(1/2)		
 	return area
 
 area = area(2,3,4)
